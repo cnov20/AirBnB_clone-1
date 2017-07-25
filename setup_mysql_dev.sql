b935434a14913477770876ed786f5f007fae913a
cnov20 <128@holbertonschool.com>
2017-07-25 15:40:25 +0000

    Added MySQL server setup script - Initial

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,5 @@
+-- Script sets up a MYSQL server
+-- Sets usage and permissions for database and user
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost'
+GRANT ALL ON *.* TO hbnb_dev_db
