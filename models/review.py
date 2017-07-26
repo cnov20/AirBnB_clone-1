85226be337ef870416d70ae4663433f36e0f35cb
cnov20 <128@holbertonschool.com>
2017-07-26 06:31:04 +0000

    Added class attributes for SQLAlchemy

--- models/review.py
+++ models/review.py
@@ -3,15 +3,23 @@
 Review Class from Models Module
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, getenv
 
 
 class Review(BaseModel):
     """Review class handles all application reviews"""
 
-    place_id = ''
-    user_id = ''
-    text = ''
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = 'reviews'
+        text = Column(String(1024), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+
+
+    else:
+        place_id = ''
+        user_id = ''
+        text = ''
 
     def __init__(self, *args, **kwargs):
         """instantiates a new review"""
