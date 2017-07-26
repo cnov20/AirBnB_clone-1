e0188cd1e3edaad3cdb6820a836129f63e54945a
cnov20 <128@holbertonschool.com>
2017-07-26 07:49:18 +0000

    Corrected attribute names and commented out method

--- models/city.py
+++ models/city.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 """
 City Class from Models Module
@@ -15,8 +16,7 @@ class City(BaseModel):
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relatiionship("Place", backref="city", cacade="all, delete,
-        delete-orphan")
+        places = relationship("Place", backref="city", cascade="all, delete, delete-orphan")
         __mapper_args = {"confirm_deleted_rows": False}
     else:
         name = ''
@@ -26,9 +26,10 @@ class City(BaseModel):
         """instantiates a new city"""
         super().__init__(self, *args, **kwargs)
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
+"""    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def place(self):
             return_places = models.storage.all("Place").values()
             end = [place for place in return_places if place.city_id = self.id]
             return end
+"""
