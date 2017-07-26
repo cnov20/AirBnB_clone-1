c35c58aa5f40738b1d0c5d99a58f7654b8d8ecd9
cnov20 <128@holbertonschool.com>
2017-07-26 06:31:40 +0000

    Added class attributes for SQLAlchemy and created relationship between City/State tables

--- models/state.py
+++ models/state.py
@@ -3,11 +3,16 @@
 State Class from Models Module
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, getenv
 
 
 class State(BaseModel):
     """State class handles all application states"""
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+      __tablename__ = 'states'
+      name = Column(String(128), nullable=False)
+      cities = relationship("City", back_populates='state', cascade="delete")
 
     name = ''
 
