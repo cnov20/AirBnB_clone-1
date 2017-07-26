a50abb96f50ef7b309db5616f1cc4a9fc2723e10
cnov20 <128@holbertonschool.com>
2017-07-26 06:32:48 +0000

    Added class attributes for SQLAlchemy and created relationship between Place/Review/User tables/classes

--- models/user.py
+++ models/user.py
@@ -3,11 +3,25 @@
 User Class from Models Module
 """
 
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base, getenv
 
 
 class User(BaseModel):
     """User class handles all application users"""
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship('Place',
+                              back_populates='user',
+                              cascade='delete')
+        reviews = relationship('Review',
+                               back_populates='user',
+                               cascade='delete')
 
     email = ''
     password = ''
