e772816e962f575a408ed2f5123742b8101ffb75
cnov20 <128@holbertonschool.com>
2017-07-28 07:12:24 +0000

    Added Task 1 - create archive of static web content, using Fabric - Initial

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''Fabric script that generates a .tgz(zipped, tar) archive
+   comprised of web static content '''
+
+from fabric.api import *
+from datetime import datetime
+from time import strftime
+
+def do_pack():
+    ''' Method archives files and returns path of archive '''
+    time_created = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    local('tar -cvzf versions/web_static_20170314233357.tgz web_static')
+    print(time_created)
+
+
+'''    if archive is not None:
+        return archive
+    else:
+        return None
+'''
