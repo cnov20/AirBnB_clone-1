8d3086f1e6da82e72ae355f9c4c80f031503a055
cnov20 <128@holbertonschool.com>
2017-07-28 05:39:11 +0000

    Removed title tag from HTML

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,6 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test
 echo -e '<html>
   <head>
-    <title>Test File</title>
   </head>
   <body>
     Holberton School
