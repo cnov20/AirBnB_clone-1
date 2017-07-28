76784750243e311ed7f2e56fa77b62327628df4f
cnov20 <128@holbertonschool.com>
2017-07-28 07:14:48 +0000

    Added condition for if tar archive is not properly created

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,12 +11,11 @@ def do_pack():
     ''' Method archives files and returns path of archive '''
     time_created = datetime.utcnow().strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
-    local('tar -cvzf versions/web_static_20170314233357.tgz web_static')
+    archive = local('tar -cvzf versions/web_static_20170314233357.tgz web_static')
     print(time_created)
 
 
-'''    if archive is not None:
+    if archive is not None:
         return archive
     else:
         return None
-'''
