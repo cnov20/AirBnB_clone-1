441b547bdeb8bf7901c388721ef4fba6911e8557
Max <mj31508@gmail.com>
2017-08-17 00:29:14 -0400

    adding new methods

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -37,57 +37,17 @@ class DBStorage:
                 obj_orm[s.__dict__["id"]] = s
         return obj_orm
 
-class FileStorage:
-    """handles long term storage of all class instances"""
-    CNC = {
-        'BaseModel': base_model.BaseModel,
-        'Amenity': amenity.Amenity,
-        'City': city.City,
-        'Place': place.Place,
-        'Review': review.Review,
-        'State': state.State,
-        'User': user.User
-    }
-    """CNC - this variable is a dictionary with:
-    keys: Class Names
-    values: Class type (used for instantiation)
-    """
-    __file_path = './dev/file.json'
-    __objects = {}
-
-    def all(self):
-        """returns private attribute: __objects"""
-        return FileStorage.__objects
-
     def new(self, obj):
-        """sets / updates in __objects the obj with key <obj class name>.id"""
-        bm_id = "{}.{}".format(type(obj).__name__, obj.id)
-        FileStorage.__objects[bm_id] = obj
+        if obj:
+            self.__session.add(obj)
 
     def save(self):
-        """serializes __objects to the JSON file (path: __file_path)"""
-        fname = FileStorage.__file_path
-        d = {}
-        for bm_id, bm_obj in FileStorage.__objects.items():
-            d[bm_id] = bm_obj.to_json()
-        with open(fname, mode='w', encoding='utf-8') as f_io:
-            json.dump(d, f_io)
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        if obj:
+            self.__session.remove()
 
     def reload(self):
-        """if file exists, deserializes JSON file to __objects, else nothing"""
-        fname = FileStorage.__file_path
-        FileStorage.__objects = {}
-        try:
-            with open(fname, mode='r', encoding='utf-8') as f_io:
-                new_objs = json.load(f_io)
-        except:
-            return
-        for o_id, d in new_objs.items():
-            k_cls = d['__class__']
-            FileStorage.__objects[o_id] = FileStorage.CNC[k_cls](**d)
-
-    def delete_all(self):
-        """deletes all stored objects, for testing purposes"""
-        del FileStorage.__objects
-        FileStorage.__objects = {}
-        self.save()
+        Base.metadata.create_all(self.__engine)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine))
