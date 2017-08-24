4a753fd6166b0ed890aa6c1683ec4be8edad03d6
cnov20 <128@holbertonschool.com>
2017-08-24 13:27:14 +0000

    Task 3 - Creates and distriubtes archive of files to web servers - Initial

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+'''Fabric script that generates a .tgz(zipped, tar) archive
+   comprised of web static content '''
+
+from fabric.api import *
+import os.path
+env.hosts = ['66.70.184.164', '142.44.164.121']
+
+
+def do_deploy(archive_path):
+
+    '''Method deploys an archive to a web server
+    and uncomporesses it to a folder '''
+
+    if (os.path.isfile(archive_path) is False):
+        return False
+
+    try:
+        ''' Unpacks compressed file / Uploads file to /tmp directory
+        of web server '''
+
+        put(archive_path, "/tmp/")
+        unpack = archive_path.split("/")[-1]
+        folder = ("/data/web_static/releases/" + unpack.split(".")[0])
+
+        run("sudo mkdir -p {:s}".format(folder))
+
+        ''' Uncompress <archive filename without extension> to created
+        folder - /data/web_static/releases/ on the web server '''
+
+        run("sudo tar -xzf /tmp/{:s} -C {:s}".format(unpack, folder))
+
+        ''' Deletes the archive from the web server '''
+        run("sudo rm /tmp/{:s}".format(unpack))
+        run("sudo mv {:s}/web_static/* {:s}/".format(folder, folder))
+        run("sudo rm -rf {:s}/web_static".format(folder))
+
+        ''' Delete the symbolic link /data/web_static/current '''
+        run('sudo rm -rf /data/web_static/current')
+
+        ''' Create a new the symbolic link /data/web_static/current on the web
+        server, linked to the new version of your code
+        (/data/web_static/releases/<archive filename without extension>) '''
+
+        run("sudo ln -s {:s} /data/web_static/current".format(folder))
+        return True
+    except:
+        return False
+
+
+def deploy():
+
+        ''' Method that creates and distributes an archive to your web servers,
+        using the function deploy '''
+
+        new_archive_path = do_pack()
+        if archive_path is None:
+            return False
+        deployed = do_deploy(new_archive_path)
+        return deployed
