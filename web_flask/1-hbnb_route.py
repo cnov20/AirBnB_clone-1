2470617650c695a419d582b5bbf1551ae4adc1b1
cnov20 <128@holbertonschool.com>
2017-08-28 11:10:10 +0000

    Added Task 1 - Add route to Flask Web App - strict_slashes option

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+''' This module uses Flask to setup a Web Server Gateway Interface
+    to a web application (HBNB) that enables the application to listen
+    for requests to it / its web server, on a given port
+
+    Defines routes and return message, indicating succesful connection
+'''
+
+from flask import Flask
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
