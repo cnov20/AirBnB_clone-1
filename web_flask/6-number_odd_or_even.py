9a07609959bfdcb00d663eb0f490d6089b4b4866
cnov20 <128@holbertonschool.com>
2017-08-28 11:32:44 +0000

    Corrected for pep8 specifications

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -13,30 +13,37 @@ from flask import Flask, url_for, render_template
 app = Flask(__name__)
 app.strict_slashes = False
 
+
 @app.route('/')
 def hello():
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb')
 def hbnb():
     return 'HBNB'
 
+
 @app.route('/c/<text>')
 def c(text):
     return 'C ' + text
 
+
 @app.route('/python/')
 @app.route('/python/<text>')
 def python(text='is cool'):
-     return 'Python ' + text.replace('_', ' ')
+    return 'Python ' + text.replace('_', ' ')
+
 
 @app.route('/number/<int:n>')
 def number(n):
     return str(n) + ' is a number'
 
+
 @app.route('/number_template/<int:n>')
 def number_template(n):
     return render_template('5-number.html', n=n)
+
 
 @app.route('/number_odd_or_even/<int:n>')
 def number_odd_or_even(n):
