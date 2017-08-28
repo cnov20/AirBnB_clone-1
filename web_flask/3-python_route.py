ac0eadb08f323cb1544efde8c15aa97e24be3dc5
cnov20 <128@holbertonschool.com>
2017-08-28 11:12:20 +0000

    Corrected for pep8 - indentation

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+''' This module uses Flask to setup a Web Server Gateway Interface
+    to a web application (HBNB) that enables the application to listen
+    for requests to it / its web server, on a given port
+
+    Defines routes and sends return message, indicating succesful connection
+'''
+
+from flask import Flask, url_for
+app = Flask(__name__)
+app.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c(text):
+    return 'C ' + text
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python(text='is cool'):
+    return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
